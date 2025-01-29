dd37a46fd14fbbe30c21c9f12eeb1c142cb6e86b
Gulnaz Artykaeva <akhmetovagrm@gmail.com>
2025-01-29 23:18:40 +0500

    Added new servise

--- api_tasks/app/main.py
+++ api_tasks/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+<<<<<<< HEAD
 from api.api_meetings import meetings_routers
 from api.api_tasks import tasks_routers
 
@@ -7,3 +8,7 @@ from api.api_tasks import tasks_routers
 app = FastAPI()
 app.include_router(tasks_routers)
 app.include_router(meetings_routers)
+=======
+
+app = FastAPI()
+>>>>>>> d0ff108 (Added new servise)
